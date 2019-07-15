7eb98073dfe25dd4c2a8ef470c7654db8fd78c1a
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-15 09:07:25 +0800

    spring注解：全注解开发，无xml;spingtest

---
+++ spring_annotation_withoutXML_springtest/src/main/java/com/zyg/service/impl/AccountServiceImpl.java
@@ -0,0 +1,44 @@
+package com.zyg.service.impl;
+
+import com.zyg.dao.IAccountDao;
+import com.zyg.domain.Account;
+import com.zyg.service.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 账户的业务层实现类
+ */
+@Service("accountService")
+public class AccountServiceImpl implements IAccountService{
+
+    @Autowired
+    private IAccountDao accountDao;
+
+    @Override
+    public List<Account> findAllAccount() {
+        return accountDao.findAllAccount();
+    }
+
+    @Override
+    public Account findAccountById(Integer accountId) {
+        return accountDao.findAccountById(accountId);
+    }
+
+    @Override
+    public void saveAccount(Account account) {
+        accountDao.saveAccount(account);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        accountDao.updateAccount(account);
+    }
+
+    @Override
+    public void deleteAccount(Integer acccountId) {
+        accountDao.deleteAccount(acccountId);
+    }
+}
